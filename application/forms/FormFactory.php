4c40c6c971553278ac518d6259bc1e9c7755ce41
tran-van-hoang <butdatac@gmail.com>
2015-09-03 16:12:22 +0700

    form factory

--- application/forms/FormFactory.php
+++ application/forms/FormFactory.php
@@ -20,28 +20,28 @@ class Application_Form_FormFactory extends Zend_Form {
      * You can search with the key word like "__name"
      * @var array This is the list of supported elements type 
      */
-    private $__validType = [
+    private $__supportedType = [
         'action',
-        'submit',
-        'name',
-        'dateOfBirth',
-        'gender',
-        'phoneNumber',
-        'email',
-        'facebook',
-        'password',
-        'note', //like information type but it can be empty
-        'information'
+        'submit',//input type = submit
+        'name', //input type= text
+        'dateOfBirth', //input type= date
+        'gender', //input type= radio
+        'phoneNumber', //input type = text
+        'number',//input type = text
+        'email', //inpyt type = text
+        'facebook', //inpyt type = text
+        'password', //inpyt type = password
+        'information', //textarea
+        'note' //like information type but it can be empty
     ];
 
     /**
      * @param string $pattern The name of form
      */
     public function __construct($formPattern) {
-        parent::__construct(null);
-
         $this->__setFormPattern($formPattern);
         $this->init($this->__assemblyMachine());
+        $this->loadDefaultDecorators();
     }
 
     /**
@@ -62,8 +62,8 @@ class Application_Form_FormFactory extends Zend_Form {
         //assembling form base on pattern,
         foreach ($pattern as $key => $value) {
             //make sure that this type is supported
-            if (in_array($key, $this->__validType)) {
-                echo "hello world";
+            if (!in_array($key, $this->__supportedType)) {
+               die($key." không được hỗ trợ");
             }
 
             $method = "__" . $key;
@@ -76,11 +76,11 @@ class Application_Form_FormFactory extends Zend_Form {
      */
     private function __getFormPattern() {
         $pattern = new $this->__formPattern();
-        
+
         if (!$pattern instanceof Application_Form_FormInterface) {
             die('This pattern must be an instance of Application_Form_FormInterface');
         }
-        
+
         return $pattern->getFormPattern();
     }
 
@@ -103,6 +103,15 @@ class Application_Form_FormFactory extends Zend_Form {
         $id = $param[2];
 
         $this->setAction($action)->setMethod($method);
+    }
+
+    /**
+     * @param array $param
+     */
+    private function __submit($param) {
+        $id = $param[0];
+
+        $this->addElement('submit', $id);
     }
 
     /**
@@ -136,165 +145,156 @@ class Application_Form_FormFactory extends Zend_Form {
         ]);
     }
 
-//
-//    /**
-//     * @param array $param
-//     */
-//    private function __email($param) {
-//        $alias = $param[0];
-//        $realName = $param[1];
-//
-//        $this->addElement('text', $alias, [
-//            'label' => ucfirst($realName) . ":",
-//            'required' => true,
-//            'validators' => [
-//                [
-//                    'NotEmpty', true, [
-//                        "messages" => [
-//                            'isEmpty' => "Bạn cần nhập $realName"
-//                        ]
-//                    ]
-//                ],
-//                [
-//                    'EmailAddress', true, [
-//                        "messages" => [
-//                            'emailAddressInvalidFormat' => "$realName nhập sai"
-//                        ]
-//                    ]
-//                ]
-//            ]
-//        ]);
-//    }
-//
-//    /**
-//     * @param array $param
-//     */
-//    private function __id($param) {
-//        $alias = $param[0];
-//        $this->addElement('hidden', $alias);
-//    }
-//
-//    /**
-//     * @param array $param
-//     */
-//    private final function __phoneNumber($param) {
-//        $alias = $param[0];
-//        $realName = $param[1];
-//
-//        $this->addElement('text', $alias, [
-//            'label' => ucfirst($realName) . ":",
-//            'required' => true,
-//            'validators' => [
-//                [
-//                    'NotEmpty', true, [
-//                        "messages" => [
-//                            'isEmpty' => "Bạn cần nhập $realName"
-//                        ]
-//                    ]
-//                ],
-//                [
-//                    'Digits', true, [
-//                        'messages' => [
-//                            'notDigits' => ucfirst($realName) 
-//                            . " chứa ký tự không phải là số"
-//                        ]
-//                    ]
-//                ],
-//                [
-//                    'StringLength', true, [
-//                        'min' => 10,
-//                        'max' => 11,
-//                        'messages' => [
-//                            'stringLengthTooShort' => ucfirst($realName) 
-//                            . " chứa ít hơn 10 số",
-//                            'stringLengthTooLong' => ucfirst($realName) 
-//                            . " chứa nhiều hơn 11 số"
-//                        ]
-//                    ]
-//                ]
-//            ]
-//        ]);
-//    }
-//
-//    /**
-//     * @param array $param
-//     */
-//    private final function __information($param) {
-//        $alias = $param[0];
-//        $realName = $param[1];
-//
-//        $this->addElement('textarea', $alias, [
-//            'label' => ucfirst($realName) . ":",
-//            'required' => true,
-//            'validators' => [
-//                [
-//                    'NotEmpty', true, [
-//                        "messages" => [
-//                            'isEmpty' => "Bạn cần nhập $realName"
-//                        ]
-//                    ]
-//                ]
-//            ]
-//        ]);
-//    }
-//
-//    /**
-//     * @param string $realName
-//     */
-//    private function __gender($realName) {
-//        
-//    }
-//
-//    /**
-//     * @param string $realName
-//     */
-//    private function __birthday($realName) {
-//        
-//    }
-//    
-//    /**
-//     * @param array $param
-//     */
-//    private  final function __anyThing($param) {
-//        $alias = $param[0];
-//        $realName = $param[1];
-//
-//        $this->addElement('text', $alias, [
-//            'label' => ucfirst($realName) . ":",
-//            'required' => true
-//        ]);
-//    }
-//
-//    /**
-//     * @param array $param
-//     */
-//    private function __number($param) {
-//        $alias = $param[0];
-//        $realName = $param[1];
-//
-//        $this->addElement('text', $alias, [
-//            'label' => ucfirst($realName) . ":",
-//            'required' => true,
-//            'validators' => [
-//                [
-//                    'Digits', true, [
-//                        'messages' => [
-//                            'notDigits' => ucfirst($realName)
-//                            . " chứa ký tự không phải là số"
-//                        ]
-//                    ]
-//                ]
-//            ]
-//        ]);
-//    }
 
     /**
      * @param array $param
      */
-    private function __submit($param) {
-        $id = $param[0];
+    private function __email($param) {
+        $alias = $param[0];
+        $realName = $param[1];
 
-        $this->addElement('submit', $id);
+        $this->addElement('text', $alias, [
+            'label' => ucfirst($realName) . ":",
+            'required' => true,
+            'validators' => [
+                [
+                    'NotEmpty', true, [
+                        "messages" => [
+                            'isEmpty' => "Bạn cần nhập $realName"
+                        ]
+                    ]
+                ],
+                [
+                    'EmailAddress', true, [
+                        "messages" => [
+                            'emailAddressInvalidFormat' => "$realName nhập sai"
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @param array $param
+     */
+    private function __id($param) {
+        $alias = $param[0];
+        $this->addElement('hidden', $alias);
+    }
+
+    /**
+     * @param array $param
+     */
+    private final function __phoneNumber($param) {
+        $alias = $param[0];
+        $realName = $param[1];
+
+        $this->addElement('text', $alias, [
+            'label' => ucfirst($realName) . ":",
+            'required' => true,
+            'validators' => [
+                [
+                    'NotEmpty', true, [
+                        "messages" => [
+                            'isEmpty' => "Bạn cần nhập $realName"
+                        ]
+                    ]
+                ],
+                [
+                    'Digits', true, [
+                        'messages' => [
+                            'notDigits' => ucfirst($realName) 
+                            . " chứa ký tự không phải là số"
+                        ]
+                    ]
+                ],
+                [
+                    'StringLength', true, [
+                        'min' => 10,
+                        'max' => 11,
+                        'messages' => [
+                            'stringLengthTooShort' => ucfirst($realName) 
+                            . " chứa ít hơn 10 số",
+                            'stringLengthTooLong' => ucfirst($realName) 
+                            . " chứa nhiều hơn 11 số"
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @param array $param
+     */
+    private final function __information($param) {
+        $alias = $param[0];
+        $realName = $param[1];
+
+        $this->addElement('textarea', $alias, [
+            'label' => ucfirst($realName) . ":",
+            'required' => true,
+            'validators' => [
+                [
+                    'NotEmpty', true, [
+                        "messages" => [
+                            'isEmpty' => "Bạn cần nhập $realName"
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @param string $realName
+     */
+    private function __gender($realName) {
+        
+    }
+
+    /**
+     * @param string $realName
+     */
+    private function __birthday($realName) {
+        
+    }
+    
+    /**
+     * @param array $param
+     */
+    private  final function __anyThing($param) {
+        $alias = $param[0];
+        $realName = $param[1];
+
+        $this->addElement('text', $alias, [
+            'label' => ucfirst($realName) . ":",
+            'required' => true
+        ]);
+    }
+
+    /**
+     * @param array $param
+     */
+    private function __number($param) {
+        $alias = $param[0];
+        $realName = $param[1];
+
+        $this->addElement('text', $alias, [
+            'label' => ucfirst($realName) . ":",
+            'required' => true,
+            'validators' => [
+                [
+                    'Digits', true, [
+                        'messages' => [
+                            'notDigits' => ucfirst($realName)
+                            . " chứa ký tự không phải là số"
+                        ]
+                    ]
+                ]
+            ]
+        ]);
     }
 
 }
